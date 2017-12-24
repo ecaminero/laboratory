5fd77ab87d916eea625752488df816f37d4cff64
Edwin <ec@Edwins-MacBook-Pro.local>
2017-12-23 23:21:09 -0300

    update scripts

--- sqlScripts/betterProducts.sql
+++ sqlScripts/betterProducts.sql
@@ -6,12 +6,3 @@ LEFT join  products AS p ON r.prod_id = p.prod_id
 GROUP BY r.prod_id, p.brand, p.name, p.price, p.cost, p.shipping_wt
 ORDER BY promedio_rating DESC, cantidad_evaluaciones DESC
 LIMIT 10;
-
-
----- ---- ---- ---- ---- ---- ---- ---- ---- ----
-SELECT avg(r.rating) AS promedio_rating, p.name AS nombre_producto, count(r.rating) AS cantidad_evaluaciones
-from ratings AS r
-left join  products AS p ON r.prod_id = p.prod_id
-GROUP BY r.prod_id, p.brand, p.name, p.price, p.cost, p.shipping_wt
-ORDER BY promedio_rating DESC
-LIMIT 10;
